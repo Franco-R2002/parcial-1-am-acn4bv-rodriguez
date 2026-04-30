f9af193229b922f49e313102bc2a1136a6632f89
Franco-R2002 <franco.erodriguez@davinci.edu.ar>
2026-04-30 19:51:39 -0300

    feat: agrego logica del boton en MainActivity

--- app/src/main/java/com/example/turnosapp/MainActivity.java
+++ app/src/main/java/com/example/turnosapp/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.turnosapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText campoNombre;
+    EditText campoEspecialidad;
+    Button botonAgregar;
+    LinearLayout contenedorTurnos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        campoNombre = findViewById(R.id.campoNombre);
+        campoEspecialidad = findViewById(R.id.campoEspecialidad);
+        botonAgregar = findViewById(R.id.botonAgregar);
+        contenedorTurnos = findViewById(R.id.contenedorTurnos);
+
+        botonAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre = campoNombre.getText().toString();
+                String especialidad = campoEspecialidad.getText().toString();
+
+                if (nombre.isEmpty() || especialidad.isEmpty()) {
+                    Toast.makeText(MainActivity.this, getString(R.string.mensaje_vacio), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                TextView nuevoTurno = new TextView(MainActivity.this);
+                nuevoTurno.setText("• " + nombre + " - " + especialidad);
+                nuevoTurno.setTextSize(16);
+                nuevoTurno.setTextColor(getResources().getColor(R.color.azul));
+                nuevoTurno.setPadding(8, 12, 8, 12);
+
+                contenedorTurnos.addView(nuevoTurno);
+
+                campoNombre.setText("");
+                campoEspecialidad.setText("");
+            }
         });
     }
 }
